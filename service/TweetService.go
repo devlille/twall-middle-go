a0cb2c547886b7f2c09e16438dc10a911837110a
François GRUCHALA <francois.gruchala@gmail.com>
2017-03-27 17:47:04 +0200

    FIX(Tweets) API
    Configuration to connect Twall to Twitter in app only mode

--- service/TweetService.go
+++ service/TweetService.go
@@ -3,10 +3,13 @@ package service
 import (
 	"context"
 
+	"golang.org/x/oauth2/clientcredentials"
+
+	log "github.com/sirupsen/logrus"
+
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/fgruchala/twall-middle-go/converter"
 	"github.com/fgruchala/twall-middle-go/model"
-	"golang.org/x/oauth2"
 )
 
 // TweetService define a tweet service
@@ -16,20 +19,32 @@ type TweetService struct {
 
 // NewTweetService initialize a connection with the Twitter API
 func NewTweetService() *TweetService {
-	config := &oauth2.Config{}
-	token := &oauth2.Token{AccessToken: "715300702298447872-ogVmHS8fX10SyxTeYRk3C9QViecTtar"}
-	httpClient := config.Client(context.Background(), token)
+	config := &clientcredentials.Config{
+		ClientID:     "XXX",
+		ClientSecret: "XXX",
+		TokenURL:     "https://api.twitter.com/oauth2/token"}
+	_, err := config.Token(context.TODO())
 
+	if err != nil {
+		log.Fatal("[TweetService] Failed to fetch Twitter API.")
+	}
+
+	httpClient := config.Client(context.TODO())
 	return &TweetService{Client: twitter.NewClient(httpClient)}
 }
 
 // Search over the Twitter API
 func (ts *TweetService) Search(params *twitter.SearchTweetParams) ([]model.Tweet, error) {
+	log.Info("[TweetService] Searching tweet(s) over Twitter API ...")
+	log.WithField("params", params).Debug("[TweetService]")
+
 	search, _, err := ts.Client.Search.Tweets(params)
 
 	if err != nil {
+		log.WithField("error", err).Error("[TweetService] Failed to search tweet(s) over Twitter API.")
 		return make([]model.Tweet, 0), err
 	}
 
+	log.WithField("count", len(search.Statuses)).Info("[TweetService] Tweet(s) returned by Twitter API.")
 	return converter.TweetsFromTwitterToTwall(search.Statuses), nil
 }
